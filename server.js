2fd9cafd478ca8b79ece7dcc67200979ccfda95e
singhharsh <haharshsingh57@gmail.com>
2026-05-01 00:08:45 +0530

    fix: Update Node version and CORS configuration

--- server.js
+++ server.js
@@ -23,16 +23,7 @@ const allowedOrigins = [
 // CORS
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow server-to-server or Postman requests
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error('Not allowed by CORS'));
-    },
+    origin: true, // Allow all origins (reflects the request origin)
     credentials: true,
   })
 );
